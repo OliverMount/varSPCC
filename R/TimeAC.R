60a262f13208d9b67bc2a6468c501da21499d0eb
OliverMount <oliverjmount@yahoo.com>
2023-12-10 18:17:27 +0900

    updating

--- R/TimeAC.R
+++ R/TimeAC.R
@@ -1,15 +1,37 @@
-TimeAC <- function(x)
-{
-
-  M <-length(x)
+#' Calculate Time-Lagged Autocorrelation
+#'
+#' This function computes the time-lagged autocorrelation of a given time series.
+#'
+#' @param x A numeric vector representing the time series.
+#'
+#' @return A numeric vector containing the time-lagged autocorrelation values.
+#'
+#' @details
+#' The function calculates the autocorrelation for different time lags.
+#' It uses a one-sided autocorrelation approach and returns a vector of autocorrelation values.
+#'
+#' @examples
+#' # Example usage:
+#' data <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+#' result <- TimeAC(data)
+#' print(result)
+#'
+#' @seealso
+#' \code{\link{acf}} for autocorrelation functions in base R.
+#'
+#' @references
+#' Insert any relevant references or citations here.
+#'
+#' @export
+TimeAC <- function(x) {
+  
+  M <- length(x)
   r <- array(0, dim = c(M))  # One-sided autocorrelation
-
-  for (i in 1:M) 
-  {
-    r[i]=(x[1:(M-i+1)]%*%x[i:M])    # Dot product in r
+  
+  for (i in 1:M) {
+    r[i] = (x[1:(M - i + 1)] %*% x[i:M])  # Dot product in r
   }
   
-  rr=c(rev(r[2:M]),r[1:M]) 
+  rr = c(rev(r[2:M]), r[1:M]) 
   return(rr/M)
-
 }
